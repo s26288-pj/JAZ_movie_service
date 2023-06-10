1fd16b51bae72f761927862efc126386fd87dbfa
s26288-pj <92212395+s26288-pj@users.noreply.github.com>
2023-06-10 16:49:13 +0200

    Delete JAZ_movie_service-main/MovieService directory

--- JAZ_movie_service-main/MovieService/src/main/java/com/example/movieservice/model/MovieCategory.java
+++
@@ -1,5 +0,0 @@
-package com.example.movieservice.model;
-
-public enum MovieCategory {
-    Action, Fantasy, Comedy
-}
--- JAZ_movie_service-main/MovieService/src/main/java/com/example/movieservice/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.movieservice.repository;
-
-import com.example.movieservice.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-
-}
